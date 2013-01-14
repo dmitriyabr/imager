5f0717af6a9f4cc743e4bdd23af36ae936313b3e
unknown <kirill.funtov@PA-KSH314.office.lamoda.ru>
2013-01-14 17:50:20 +0400

    nother debug

--- downloader.php
+++ downloader.php
@@ -14,10 +14,11 @@ $config=parse_ini_file('config.ini');
     foreach($xml->xpath($config['PICTURES_XPATH']) as $picture){
         $tmp_img=$config['TMP_IMAGE_PATH'].$counter.'.jpg';
         $tmp_info=$config['TMP_INFO_PATH'].$counter.'.inf';
-        exec("wget $picture -O $tmp_img -o /dev/null");
+       // exec("wget $picture -O $tmp_img -o /dev/null");
+        exec("wget $picture -O $tmp_img");
         $path=substr($picture, $config['PATH_START']);
         $mtime=filemtime($tmp_img);
         file_put_contents($tmp_info,$path."\n".$mtime);
         $counter++;
-        echo "$counter\n";
+        //echo "$counter\n";
     }
