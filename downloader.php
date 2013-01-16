2866cd6c18fe6cb32e2989f003185ef3de7a8ac7
funtaps <funtaps@gmail.com>
2013-01-16 14:14:50 +0400

    downloader args

--- downloader.php
+++ downloader.php
@@ -24,8 +24,8 @@ $xml=new SimpleXMLElement(file_get_contents($config['XML_FILE_NAME']));
 $counter=$staring_counter;
 
 foreach($xml->xpath($config['PICTURES_XPATH']) as $picture){
-    if($counter<$staring_counter+$pass_count){
-        $counter++;
+    if($pass_count>0){
+        $pass_count--;
         continue;
     }
     $tmp_img=$config['TMP_IMAGE_PATH'].$counter.'.jpg';
@@ -40,5 +40,5 @@ foreach($xml->xpath($config['PICTURES_XPATH']) as $picture){
     $mtime=filemtime($tmp_img);
     file_put_contents($tmp_info,$path."\n".$mtime);
     $counter++;
-    echo "$counter\n";
+    echo ($counter-$staring_counter)."\n";
 }
