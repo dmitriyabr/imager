99ceb68a520b1219aa14ba7f544b1dc787de8d8b
funtaps <funtaps@gmail.com>
2013-03-19 18:59:38 +0400

    Update resizer2.php

--- resizer2.php
+++ resizer2.php
@@ -6,6 +6,13 @@
  * Time: 13:21
  * To change this template use File | Settings | File Templates.
  */
+$child_pid = pcntl_fork();
+if ($child_pid) {
+    // Выходим из родительского, привязанного к консоли, процесса
+    exit();
+}
+// Делаем основным процессом дочерний.
+posix_setsid();
 
 include 'Resizer.php';
 include 'Picture.php';
@@ -32,5 +39,13 @@ function resize_recursive($dir_name,$rs, $config){
 }
 
 $config=parse_ini_file('config2.ini');
+
+ini_set('error_log',$config['ERROR_LOG']);
+fclose(STDIN);
+fclose(STDOUT);
+fclose(STDERR);
+$STDIN = fopen('/dev/null', 'r');
+$STDOUT = fopen($config['APPLICATION_LOG'], 'ab');
+$STDERR = fopen($config['STDERR_LOG'], 'ab');
 $rs=new Resizer($config['TARGET_WIDTH'],$config['TARGET_HEIGHT'],array('red'=>255,'green'=>255,'blue'=>255));
 resize_recursive($config['SOURCE_DIR'],$rs, $config);
